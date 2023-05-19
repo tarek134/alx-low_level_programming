91c822b3e44e736abdac459950096293ca672246
tarek134 <tarekmagdi178@gmail.com>
2023-05-19 15:06:27 -0700

    fix code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+ *i make a type one line using puts function 
+ *and make a return 0  that meaning succssful */
+
 int main(void)
 {
+	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
