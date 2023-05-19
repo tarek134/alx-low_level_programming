a84ddbb190e2833e48ba1fbede30ef2c3772f579
tarek134 <tarekmagdi178@gmail.com>
2023-05-18 19:59:48 -0700

    3-name

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{
+	puts("Programming");
+	puts("is");
+	puts("like");
+	puts("building");
+	puts("a");
+	puts("multilingual");
+	puts("puzzle");
+	puts("\n");
+	return (0);
+}
