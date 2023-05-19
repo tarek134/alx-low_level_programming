b5964d7e8708fdeaf5820f9df128ff6a3b058340
tarek134 <tarekmagdi178@gmail.com>
2023-05-18 20:10:28 -0700

    3-name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 int main()
 {
-	puts("Programming");
-	puts("is");
-	puts("like");
-	puts("building");
-	puts("a");
-	puts("multilingual");
-	puts("puzzle");
-	puts("\n");
+	puts("\""Programming is like building a multilingual puzzle")
 	return (0);
 }
