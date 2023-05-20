37b68e5684bdbb9a03f9db70e84984769c76862e
tarek134 <tarekmagdi178@gmail.com>
2023-05-20 09:33:37 -0700

    fix code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /**
-*main - Entry point
-*
-*Description: A c progrme that prints with puts function
-*
-*Return: Alawys 0 (Success)
+ *main - Entry point
+ *
+ *Description: A c progrme that prints with puts function
+ *
+ *Return: Alawys 0 (Success)
 */
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
