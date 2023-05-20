db04d2fd677e26e9ccdf4da8e0d9e868edd3f6e2
tarek134 <tarekmagdi178@gmail.com>
2023-05-20 15:01:23 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: this is c program that print one line with print function
+ *
+ * Return: shoud be 0 to (success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
